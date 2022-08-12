40d7185a70176b28f7330cbeb1b16bcae17201f7
Yolis <yolisisabel28@gmail.com>
2022-08-11 19:58:49 -0700

    adjusted img and links

--- portfolio/self-project.js
+++ portfolio/self-project.js
@@ -1,5 +1,13 @@
 function showAlert() {
   alert("Lets get to know me better :)");
 }
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
 
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
 //this is a test
