2ecd83ae45804e9847c7504302585341496f70df
Yolis <yolisisabel28@gmail.com>
2022-09-15 18:49:19 -0700

    identifying corr/incorrect ans

--- week8Game/client/src/components/answer.js
+++ week8Game/client/src/components/answer.js
@@ -1,14 +1,18 @@
 import { useState } from "react";
 
+//props is an obj with a key of answer inside of it
+//passby reference:
 const Answer = (props) => {
   const [currentClass, setCurrentClass] = useState("blue");
 
-  const onClick = () => {
-    setCurrentClass(props.allAnswers === props.allAnswers[0] ? "green" : "red");
+  const handleOnClick = () => {
+    setCurrentClass(props.answer === props.correctAnswer ? "green" : "red");
   };
   return (
     <p>
-      <input className={currentClass} type="radio" />
+      <button className={currentClass} onClick={handleOnClick}>
+        {props.answer}
+      </button>
     </p>
   );
 };
--- week8Game/client/src/components/question.js
+++ week8Game/client/src/components/question.js
@@ -6,14 +6,23 @@ import Answer from "./answer.js";
 //getting correctText ("string") and incorrecText (arr of string), stored in allAnswers var
 //10 display q text
 //map method is iterating through each answer (all in p tag which has radio button)
+//... spread operator; action is called destructure
+//without ... it would be arr within arr
+//taking out values from incorrect array and then adding to new array which is allAnswers
+// question component is destructuring the props
+
 function Question({ questionText, correctText, incorrectText }) {
   const allAnswers = [correctText, ...incorrectText];
+
+  const animal = () => {};
+
   return (
     <div>
       {questionText}
-      {allAnswers.map((answer, index) => {
+      {allAnswers.map((ans, index) => {
         return (
-          <Answer answer={answer} key={index} />
+          <Answer answer={ans} correctAnswer={correctText} key={index} />
+
           // <p key={answer}>
           //   <input type="radio" value={answer} />
           //   {answer}
