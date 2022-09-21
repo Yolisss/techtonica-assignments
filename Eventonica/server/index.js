ad681969b3d09b640333a9f2af3537a09775b134
Yolis <yolisisabel28@gmail.com>
2022-09-20 18:55:51 -0700

    created server

--- Eventonica/server/index.js
+++ Eventonica/server/index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import cors from "cors";
+import usersRouter from "./routes/users.js";
 
 const app = express();
 const PORT = 4000;
 
 app.use(cors());
+
+app.use("/users", usersRouter);
 
 let mockUsers = [
   { id: 1, name: "Marlin", email: "marlin@gmail.com" },
--- Eventonica/server/routes/users.js
+++ Eventonica/server/routes/users.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+// server/routes/ users.mjs;
+import express from "express";
+const router = express.Router();
+import db from "../db/db-connection.js";
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+router.get("/", async function (req, res, next) {
+  try {
+    const users = await db.any("SELECT * FROM users", [true]);
+    res.send(users);
+  } catch (e) {
+    return res.status(400).json({ e });
+  }
 });
 
-module.exports = router;
+/* post request goes here */
+
+/* delete request goes here  */
+
+export default router;
