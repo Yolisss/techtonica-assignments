1b41491f69219fbd091f424cd8278b243c5991ca
Yolis <yolisisabel28@gmail.com>
2022-09-20 18:53:09 -0700

    removed node modules

---
+++ Eventonica/server/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = 4000;
+
+app.use(cors());
+
+let mockUsers = [
+  { id: 1, name: "Marlin", email: "marlin@gmail.com" },
+  { id: 2, name: "Nemo", email: "nemo@gmail.com" },
+  { id: 3, name: "Dory", email: "dory@gmail.com" },
+];
+
+// server/routes/users.mjs`
+app.get("/", function (req, res, next) {
+  console.log(req.body, "the body");
+  res.json({ users: mockUsers });
+});
+
+app.listen(PORT, () => console.log(`Hola! Server is running on port ${PORT}`));
